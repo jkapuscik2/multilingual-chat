cf61aa798ae483d64be4ecd4177b6851b3ad4190
kuba <j.kapuscik@abyssglass.pl>
2019-09-19 20:08:21 +0200

    Basic server setup

---
+++ server/events.js
@@ -0,0 +1,15 @@
+const CONNECTED = 'connection'
+const LOGGED_IN = 'logged-in';
+const CHOSEN_LANG = 'chosen-lang';
+const SENT_MSG = 'sent-msg';
+const DISCONNECTED = 'disconnect';
+const UPDATED_USERS = 'updated-users';
+
+module.exports = {
+    CONNECTED,
+    LOGGED_IN,
+    CHOSEN_LANG,
+    SENT_MSG,
+    DISCONNECTED,
+    UPDATED_USERS
+}
---
+++ server/server.js
@@ -0,0 +1,46 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cors = require('cors');
+const EVENTS = require("./events");
+const PORT = 8080;
+
+app.use(cors());
+server.listen(PORT, () => console.log(`Connected to port ${PORT}!`));
+
+let users = {};
+
+const getActiveUSers = () => {
+    let activeUsers = [];
+
+    Object.keys(users).forEach((idx) => {
+        const user = users[idx];
+        if (user.name && user.lang) {
+            activeUsers.push(user)
+        }
+    });
+
+    return activeUsers;
+}
+
+io.on(EVENTS.CONNECTED, socket => {
+    users[socket.id] = {
+        id: socket.id
+    };
+
+    socket.on(EVENTS.DISCONNECTED, () => {
+        delete users[socket.id];
+
+        io.sockets.emit(EVENTS.UPDATED_USERS, users)
+    });
+
+    socket.on(EVENTS.LOGGED_IN, name => {
+        users[socket.id]['name'] = name
+    });
+
+    socket.on(EVENTS.CHOSEN_LANG, lang => {
+        users[socket.id]['lang'] = lang;
+
+        io.sockets.emit(EVENTS.UPDATED_USERS, getActiveUSers())
+    });
+});
